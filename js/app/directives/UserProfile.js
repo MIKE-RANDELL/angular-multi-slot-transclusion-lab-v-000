7137ebc98b1750a77ce64d90a4a55983dff172af
MIKE-RANDELL <michael.james.randell@gmail.com>
2017-02-15 15:48:02 -0500

    Done.

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,12 +1,21 @@
 function UserProfile() {
 	return {
-		transclude: true,
+		transclude: {
+			name: 'h4',
+			position: 'h6',
+			description: 'p'
+		},
 		template: [
 			'<div>',
 				'<h3>User Profile</h3>',
-				'<div ng-transclude></div>',
+				'<div>Name:<span ng-transclude="name"></span></div>',
+				'<div>Position:<span ng-transclude="position"></span></div>',
+				'<div>Description: <span ng-transclude="description"></span></div>',
 			'</div>'
 		].join('')
+		//link: function(scope, elem, attrs, ctrl, transclude){
+
+		//};
 	}
 }
 
